e1ddfc7d2e1e7b6b363f3f8e4850fe460d182caf
Michael-Wagner459 <154291649+Michael-Wagner459@users.noreply.github.com>
2024-01-14 14:09:16 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// This is how old I am
+var myAge = 30
+
+// early years
+var earlyYears = 2
+
+earlyYears = earlyYears * 10.5
+
+// This is my age minus 2
+var laterYears = myAge - 2
+
+laterYears = laterYears * 4
+
+// This is my age in dog years
+var myAgeInDogYears = earlyYears + laterYears
+
+// this is my name with all lower case letters
+var myName = ('Michael').toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
